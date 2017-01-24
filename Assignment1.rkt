ccf7777202ff1d7a7640a59a34333917ada0bd5e
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-01-24 04:00:57 -0500

    Initialization
    Initialized the function Reverse general and implemented the empty list
    test case

---
+++ Assignment1.rkt
@@ -0,0 +1,9 @@
+
+(DEFINE (reverse-general list) ;initializing the initial function
+        (if (null? list)
+            (QUOTE(list)) ;if the list is empty return the list
+            )
+        )
+
+
+       
