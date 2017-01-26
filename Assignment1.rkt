d4a26076d53dd0c6f1fd951f114e09d195669087
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-01-26 02:58:51 -0500

    parameter name change
    changed exe to L , and initialized the next function.

--- Assignment1.rkt
+++ Assignment1.rkt
@@ -1,11 +1,13 @@
 
-(DEFINE (reverse-general exe) ;initializing the initial function
-        (cond ( (null? exe) '()) ;Conditional function that returns an empty list if list is empty
-              ((list? exe) ; second condition if the example is a lift do the conditions below  
-               (append (reverse-general (cdr exe)) ; appends the first elment in the list by recursively calling the function to check if the first element is blank or if its another list and if it is it calls the function again
-                       (list (reverse-general(car exe))) ;adds all the atoms starting from the back of the list exe to the new list 
+(DEFINE (reverse-general L) ;initializing the initial function
+        (cond ( (null? L) '()) ;Conditional function that returns an empty list if list is empty
+              ((list? L) ; second condition if the example is a lift do the conditions below  
+               (append (reverse-general (cdr L)) ; appends the first elment in the list by recursively calling the function to check if the first element is blank or if its another list and if it is it calls the function again
+                       (list (reverse-general(car L))) ;adds all the atoms starting from the back of the list exe to the new list 
                 ) 
                )
-              (else exe) ; Returns the list 
+              (else L) ; Returns the list 
          )
 )
+
+(DEFINE (sum-up-numbers-simple L))
