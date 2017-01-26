317b15c0a7123734076e68c5ef6e23d34cfc3665
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-01-26 02:56:02 -0500

    modified the conditions and commented the code
    modified the condition, as well as the name of the list.

--- Assignment1.rkt
+++ Assignment1.rkt
@@ -1,4 +1,11 @@
 
-(DEFINE (reverse-general list) ;initializing the initial function
-        (cond ( (null? list) '())) ;Conditional function that returns an empty list if list is empty
-      )
+(DEFINE (reverse-general exe) ;initializing the initial function
+        (cond ( (null? exe) '()) ;Conditional function that returns an empty list if list is empty
+              ((list? exe) ; second condition if the example is a lift do the conditions below  
+               (append (reverse-general (cdr exe)) ; appends the first elment in the list by recursively calling the function to check if the first element is blank or if its another list and if it is it calls the function again
+                       (list (reverse-general(car exe))) ;adds all the atoms starting from the back of the list exe to the new list 
+                ) 
+               )
+              (else exe) ; Returns the list 
+         )
+)
